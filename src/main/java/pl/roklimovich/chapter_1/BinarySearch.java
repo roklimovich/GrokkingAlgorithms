fea2158c1a90ada57cb3b93897a56efa13b2c419
Roman Klimovich <ro.klimovich@gmail.com>
2024-09-30 16:32:18 +0200

    Implemented algorithm for chapter 1

---
+++ src/main/java/pl/roklimovich/chapter_1/BinarySearch.java
@@ -0,0 +1,35 @@
+package pl.roklimovich.chapter_1;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        int key = 6;
+        System.out.println(binarySearch(array, key));
+
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int key1 = 10;
+        System.out.println(binarySearch(array1, key1));
+    }
+
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
---
+++ src/test/java/chapter_1/BinarySearchTest.java
@@ -0,0 +1,61 @@
+package chapter_1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.roklimovich.chapter_1.BinarySearch;
+
+public class BinarySearchTest {
+
+    @Test
+    public void binarySearchTest() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int key = 4;
+        int expectedIndex = 3;
+        int foundIndex = BinarySearch.binarySearch(array, key);
+
+        Assertions.assertEquals(expectedIndex, foundIndex);
+    }
+
+    @Test
+    public void binarySearchMiddleTest() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int key = 3;
+        int expectedIndex = 2;
+        int foundIndex = BinarySearch.binarySearch(array, key);
+
+        Assertions.assertEquals(expectedIndex, foundIndex);
+    }
+
+
+    @Test
+    public void binarySearchAtBeginTest() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int key = 1;
+        int expectedIndex = 0;
+        int foundIndex = BinarySearch.binarySearch(array, key);
+
+        Assertions.assertEquals(expectedIndex, foundIndex);
+    }
+
+    @Test
+    public void binarySearchAtEndTest() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int key = 6;
+        int expectedIndex = 5;
+        int foundIndex = BinarySearch.binarySearch(array, key);
+
+        Assertions.assertEquals(expectedIndex, foundIndex);
+    }
+
+
+    @Test
+    public void binarySearchNotFoundTest() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int key = 10;
+        int expectedIndex = -1;
+        int foundIndex = BinarySearch.binarySearch(array, key);
+
+        Assertions.assertEquals(expectedIndex, foundIndex);
+    }
+
+}
